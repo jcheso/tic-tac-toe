59f98d083f2b61cce88631f38d9ae59e0f135e22
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-23 22:05:21 +0800

    Added user name enty and draw check.

--- scripts.js
+++ scripts.js
@@ -2,33 +2,88 @@ const Player = (symbol, name) => {
   const playerWin = (board) => {
     // let symbol = currentPlayer.symbol;
     let playerWin = null;
-    if (board[0] == symbol && board[1] == symbol && board[2] == symbol) {
-      playerWin = true;
-    } else if (board[3] == symbol && board[4] == symbol && board[5] == symbol) {
-      playerWin = true;
-    } else if (board[6] == symbol && board[7] == symbol && board[8] == symbol) {
-      playerWin = true;
-    } else if (board[0] == symbol && board[4] == symbol && board[8] == symbol) {
-      playerWin = true;
-    } else if (board[2] == symbol && board[4] == symbol && board[6] == symbol) {
-      playerWin = true;
-    } else if (board[0] == symbol && board[3] == symbol && board[6] == symbol) {
-      playerWin = true;
-    } else if (board[1] == symbol && board[4] == symbol && board[7] == symbol) {
-      playerWin = true;
-    } else if (board[2] == symbol && board[5] == symbol && board[8] == symbol) {
-      playerWin = true;
+    let gameDraw = null;
+    // Check for draw - i.e check no blank spaces left on board
+    if (board.includes("")) gameDraw = false;
+    else {
+      gameDraw = true;
+      document.getElementById("winner-text").innerText =
+        "It's a draw, play again!";
+      gameBoard.clearBoard();
     }
-    return playerWin;
+
+    if (!gameDraw) {
+      if (board[0] == symbol && board[1] == symbol && board[2] == symbol) {
+        playerWin = true;
+      } else if (
+        board[3] == symbol &&
+        board[4] == symbol &&
+        board[5] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[6] == symbol &&
+        board[7] == symbol &&
+        board[8] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[0] == symbol &&
+        board[4] == symbol &&
+        board[8] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[2] == symbol &&
+        board[4] == symbol &&
+        board[6] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[0] == symbol &&
+        board[3] == symbol &&
+        board[6] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[1] == symbol &&
+        board[4] == symbol &&
+        board[7] == symbol
+      ) {
+        playerWin = true;
+      } else if (
+        board[2] == symbol &&
+        board[5] == symbol &&
+        board[8] == symbol
+      ) {
+        playerWin = true;
+      }
+
+      return playerWin;
+    }
   };
   return { symbol, playerWin, name };
 };
 
 const gameBoard = (() => {
   var board = ["", "", "", "", "", "", "", "", ""];
-  let playerX = Player("X", "Jarryd");
-  let playerO = Player("O", "Ethan");
-  let currentPlayer = playerX;
+  var currentPlayer;
+  const setPlayerNames = (playerXName, playerOName) => {
+    playerX = Player("X", playerXName);
+    playerO = Player("O", playerOName);
+    console.log(playerX.name);
+    currentPlayer = playerX;
+    document.getElementById("winner-text").innerText =
+      "Good luck " +
+      playerXName +
+      " and " +
+      playerOName +
+      ", " +
+      playerXName +
+      " goes first!";
+    displayController.activateBoard();
+    return currentPlayer;
+  };
 
   const clearBoard = () => (board = ["", "", "", "", "", "", "", "", ""]);
 
@@ -47,44 +102,55 @@ const gameBoard = (() => {
     }
   };
   return {
+    setPlayerNames,
     clearBoard,
     updateBoard,
   };
 })();
 
 const displayController = (() => {
-  // Add event listeners
-  for (let index = 1; index < 10; index++) {
-    boardElement = document.getElementById(index.toString());
-    boardElement.addEventListener("click", function (event) {
-      let index = this.id;
-      gameBoard.updateBoard(index);
-      event.target.setAttribute("class", "board-element-click");
-    });
-  }
+  const setPlayerNames = () => {
+    document
+      .getElementById("players-form")
+      .addEventListener("submit", (event) => {
+        let playerXName = document.getElementById("playerX").value;
+        let playerYName = document.getElementById("playerO").value;
+        gameBoard.setPlayerNames(playerXName, playerYName);
+        event.preventDefault();
+      });
+  };
+
   const activateBoard = () => {
     for (let index = 1; index < 10; index++) {
       boardElement = document.getElementById(index.toString());
       boardElement.addEventListener("click", function (event) {
         let index = this.id;
         gameBoard.updateBoard(index);
+        event.target.setAttribute("class", "board-element-click");
       });
     }
   };
+
   const updateBoardElement = (index, currentPlayer) =>
     (document.getElementById(index.toString()).innerText =
       currentPlayer.symbol);
 
   const clearBoard = () => {
     for (let index = 1; index < 10; index++) {
+      document
+        .getElementById(index.toString())
+        .setAttribute("class", "board-element");
       document.getElementById(index.toString()).innerText = "";
       gameBoard.clearBoard();
     }
   };
+
   const announceWinner = (currentPlayer) => {
     document.getElementById("winner-text").innerText =
       "The winner is " + currentPlayer;
   };
+
+  setPlayerNames();
 
   return {
     announceWinner,
